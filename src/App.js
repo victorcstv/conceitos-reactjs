4962e7494638f25fc4446a5c0db602d307512f34
Victor Costa <victorcstv@gmail.com>
2020-08-31 23:28:24 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => setRepositories(response.data))
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('/repositories', {
+      title: `Novo projeto ${Date.now()}`,
+      url: "https://github.com/victorcstv",
+      techs: ["ReactJS", "React Native"]
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`/repositories/${id}`);
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    const newRepositories = repositories;
+    newRepositories.splice(repositoryIndex, 1);
+
+    setRepositories([...newRepositories]);
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+
+        {repositories.map(repository =>
+          <li key={repository.id}>
+            {repository.title}
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>)}
+
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
